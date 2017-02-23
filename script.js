8e07918f83764d7d6cc34b05fd64afeb0c75f5eb
Thomas-Redding <mtredding@yahoo.com>
2017-02-22 18:17:54 -0600

    table of contents added

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ var foo;
 onload = function() {
 	var url = window.location.href;
 	path = url.substr(url.indexOf("nice-notes") + 11).replace("-", " ").split("/");
+    var file;
 	if (path.length == 1) {
 		// index.html
+        file = path[0];
 		console.log("index.html");
 		header_html += "<a href=\"index.html\">Nice Notes</a>";
 	}
 	else {
 		var folder = path[0];
-		var file = path[1];
+		file = path[1];
 		header_html += "<a href=\"../index.html\">Nice Notes</a>";
 		if (path[1] == "index.html") {
 			// linear-algebra/index.html
@@ -30,10 +32,81 @@ onload = function() {
 	}
 
 	var main = document.getElementsByTagName("main")[0];
+
 	var header = document.createElement("header");
 	header.innerHTML = header_html;
+    document.body.insertBefore(header, document.body.firstChild)
+
+
 	var footer = document.createElement("footer");
 	footer.innerHTML = footer_html;
-	document.body.insertBefore(header, document.body.firstChild)
-	main.parentNode.insertBefore(footer, main.nextSibling);
+    main.parentNode.insertBefore(footer, main.nextSibling);
+
+    if (file == "index.html")
+        return;
+
+    var nav = document.createElement("nav");
+    var h2s = document.getElementsByTagName("h2");
+    var h3s = document.getElementsByTagName("h3");
+    var headers = [];
+    for (var i = 0; i < h2s.length; ++i) headers.push({
+        element: h2s[i],
+        y: get_y_pos(h2s[i]),
+        level: 2
+    });
+    for (var i = 0; i < h3s.length; ++i) headers.push({
+        element: h3s[i],
+        y: get_y_pos(h3s[i]),
+        level: 3
+    });
+
+    headers.sort(function (a, b) {
+        return a.y > b.y
+    });
+
+    var level = 2;
+    var str = "<ol>";
+    for (var i = 0; i < headers.length; ++i) {
+        if (headers[i].level > level) str += "<ol>";
+        else if (headers[i].level < level) str += "</ol>";
+        level = headers[i].level;
+        str += "<li><a href='#' onclick='go_to_header(" + i + "); return false;'>" + headers[i].element.innerHTML + "</a></li>";
+    }
+    str += "</ol>";
+    nav.innerHTML = str;
+
+    var title = document.getElementsByTagName("h1")[0];
+
+	main.insertBefore(nav, title.nextElementSibling);
+}
+
+function get_y_pos(el) {
+    // http://stackoverflow.com/questions/288699/get-the-position-of-a-div-span-tag
+
+    var y;
+    for (y = 0; el != null; el = el.offsetParent)
+        y += el.offsetTop;
+    return y;
+}
+
+function go_to_header(num) {
+    var h2s = document.getElementsByTagName("h2");
+    var h3s = document.getElementsByTagName("h3");
+    var headers = [];
+    for (var i = 0; i < h2s.length; ++i) headers.push({
+        element: h2s[i],
+        y: get_y_pos(h2s[i]),
+        level: 2
+    });
+    for (var i = 0; i < h3s.length; ++i) headers.push({
+        element: h3s[i],
+        y: get_y_pos(h3s[i]),
+        level: 3
+    });
+    headers.sort(function (a, b) {
+        return a.y > b.y
+    });
+
+	headers[num].element.scrollIntoView();
+	window.scrollBy(0, -50);
 }
